4c910d3e775d6e6cf094bad2aa3061df07f78de9
github username <github email address>
2019-03-02 15:13:03 -0500

    done

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ return num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+
+def argue(name)
+  return name
 end
 
-def greeting
-
+def greeting(name, language)
+  puts "Hello, name. We heard you are good at language"
 end
+
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(name = "cheese")
+ return name
 end
